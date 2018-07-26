6711724ae9afc52a1ad35af52b2b8dc7d4d74fb5
Victor Otieno Omondi <vickz84259@gmail.com>
2018-07-27 02:26:21 +0300

    Handle configuration changes
    This is through storing the query string in a Bundle
    
    Signed-off-by: Victor Otieno Omondi <vickz84259@gmail.com>

--- app/src/main/java/ke/co/slick/researchapp/ui/searchresults/SearchResultsActivity.kt
+++ app/src/main/java/ke/co/slick/researchapp/ui/searchresults/SearchResultsActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import ke.co.slick.researchapp.R
 import ke.co.slick.researchapp.ResearchApplication
+import ke.co.slick.researchapp.ui.EXTRA_QUERY
 import javax.inject.Inject
 
 class SearchResultsActivity : AppCompatActivity(),
@@ -11,6 +12,7 @@ class SearchResultsActivity : AppCompatActivity(),
 
     @Inject
     override lateinit var presenter: SearchResultsContract.Presenter
+    lateinit var query: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         val app = application as ResearchApplication
@@ -18,5 +20,17 @@ class SearchResultsActivity : AppCompatActivity(),
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search_results)
+
+        query = intent.getStringExtra(EXTRA_QUERY)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("query", query)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        query = savedInstanceState.getString("query")
     }
 }

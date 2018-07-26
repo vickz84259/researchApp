49460359d47c7d54e2058fc99f46d568d2cbf63e
Victor Otieno Omondi <vickz84259@gmail.com>
2018-07-26 03:17:47 +0300

    Created SearchResultsActivity
    This activity will display the results of the api call using the query
    given from the MainActivity
    
    Signed-off-by: Victor Otieno Omondi <vickz84259@gmail.com>

---
+++ app/src/main/java/ke/co/slick/researchapp/ui/SearchResultsActivity.kt
@@ -0,0 +1,13 @@
+package ke.co.slick.researchapp.ui
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import ke.co.slick.researchapp.R
+
+class SearchResultsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_results)
+    }
+}

44374ad820e363e9117e958239902d491eadee10
Victor Otieno Omondi <vickz84259@gmail.com>
2018-07-26 03:11:42 +0300

    Added logic for button in MainActivity
    Signed-off-by: Victor Otieno Omondi <vickz84259@gmail.com>

--- app/src/main/java/ke/co/slick/researchapp/ui/MainActivity.kt
+++ app/src/main/java/ke/co/slick/researchapp/ui/MainActivity.kt
@@ -1,13 +1,27 @@
 package ke.co.slick.researchapp.ui
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import ke.co.slick.researchapp.R
+import kotlinx.android.synthetic.main.activity_main.*
+
+const val EXTRA_QUERY = "ke.co.slick.researchapp.QUERY"
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun sendQuery(view: View) {
+        val query = editText.text.toString()
+        val intent = Intent(this, SearchResultsActivity::class.java).apply {
+            putExtra(EXTRA_QUERY, query)
+        }
+
+        startActivity(intent)
     }
 }

77b673ee616ffcf4749a5b0a9b27e81c2e7ba556
Victor Otieno Omondi <vickz84259@gmail.com>
2018-07-28 05:55:36 +0300

    Add cache capabilities
    Implemented a CacheManager that caches responses to the
    SharedPreferences. This will help in handling configuration changes.
    
    Signed-off-by: Victor Otieno Omondi <vickz84259@gmail.com>

--- app/src/main/java/ke/co/slick/researchapp/data/CacheManager.kt
+++ app/src/main/java/ke/co/slick/researchapp/data/CacheManager.kt
@@ -2,8 +2,22 @@ package ke.co.slick.researchapp.data
 
 import android.content.SharedPreferences
 import javax.inject.Inject
+import javax.inject.Singleton
 
+@Singleton
 class CacheManager @Inject constructor(private val sharedPreferences: SharedPreferences) {
 
+    fun isCacheAvailable(query: String) = sharedPreferences.contains(query)
 
+    fun getCache(query: String) = sharedPreferences.getString(query, "")
+
+    fun storeCache(query: String, value: String) {
+        sharedPreferences.edit().putString(query, value).apply()
+    }
+
+    fun clearCache(query: String?) {
+        val editor = sharedPreferences.edit()
+
+        if (query is String) editor.remove(query).apply() else editor.clear().apply()
+    }
 }
--- app/src/main/java/ke/co/slick/researchapp/data/DataManager.kt
+++ app/src/main/java/ke/co/slick/researchapp/data/DataManager.kt
@@ -1,11 +1,34 @@
 package ke.co.slick.researchapp.data
 
+import com.squareup.moshi.Moshi
+import io.reactivex.Observable
 import ke.co.slick.researchapp.data.apis.UsptoApi
+import ke.co.slick.researchapp.data.models.ApiResponse
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class DataManager @Inject constructor(val usptoApi: UsptoApi) {
+class DataManager @Inject constructor(val usptoApi: UsptoApi, val cacheManager: CacheManager) {
 
-    fun getSearchResults(query: String) = usptoApi.search(query)
+    private val moshi = Moshi.Builder().build()
+    private val jsonAdapter = moshi.adapter(ApiResponse::class.java)
+
+    private fun getFromCache(query: String): ApiResponse {
+        val jsonData = cacheManager.getCache(query)
+        return jsonAdapter.fromJson(jsonData) ?: throw NullPointerException()
+    }
+
+    private fun storeInCache(query: String, apiResponse: ApiResponse) {
+        val jsonData = jsonAdapter.toJson(apiResponse)
+        cacheManager.storeCache(query, jsonData)
+    }
+
+    fun getSearchResults(query: String): Observable<ApiResponse> {
+        val modifiedQuery = "uspto:$query"
+
+        return when (cacheManager.isCacheAvailable(modifiedQuery)) {
+            true -> Observable.just(getFromCache(modifiedQuery))
+            false -> usptoApi.search(query).doOnNext { storeInCache(modifiedQuery, it) }
+        }
+    }
 }
